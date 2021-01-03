2486e758fa6f0e5a7968fdef59f3669e5cc31969
Roland Bernard <rolli.bernard@gmail.com>
2021-01-03 22:36:06 +0100

    Added some vector functions

---
+++ src/vec.c
@@ -0,0 +1,116 @@
+
+#include <math.h>
+
+#include "vec.h"
+
+Vec3 createVec3(float x, float y, float z) {
+    Vec3 ret = { .x = x, .y = y, .z = z };
+    return ret;
+}
+
+Vec3 scaleVec3(Vec3 v, float s) {
+    return createVec3(v.x * s, v.y * s, v.z * s);
+}
+
+Vec3 addVec3(Vec3 u, Vec3 v) {
+    return createVec3(u.x * v.x, u.y * v.y, u.z * v.z);
+}
+
+float dotVec3(Vec3 u, Vec3 v) {
+    return (u.x * v.x) + (u.y * v.y) + (u.z * v.z);
+}
+
+Vec3 crossVec3(Vec3 u, Vec3 v) {
+    return createVec3(
+        (u.y * v.z) + (u.z * v.y),
+        (u.z * v.x) + (u.x * v.z),
+        (u.x * v.y) + (u.y * v.x)
+    );
+}
+
+Mat3x3 createNullMat3x3() {
+    Mat3x3 ret = { .v = {
+        { 0, 0, 0, },
+        { 0, 0, 0, },
+        { 0, 0, 0, },
+    }};
+    return ret;
+}
+
+Mat3x3 createIdentMat3x3() {
+    Mat3x3 ret = { .v = {
+        { 1, 0, 0, },
+        { 0, 1, 0, },
+        { 0, 0, 1, },
+    }};
+    return ret;
+}
+
+Mat3x3 createRotationXMat3x3(float a) {
+    Mat3x3 ret = { .v = {
+        { 1,      0,       0, },
+        { 0, cos(a), -sin(a), },
+        { 0, sin(a),  cos(a), },
+    }};
+    return ret;
+}
+
+Mat3x3 createRotationYMat3x3(float a) {
+    Mat3x3 ret = { .v = {
+        {  cos(a), 0, sin(a), },
+        {       0, 1,      0, },
+        { -sin(a), 0, cos(a), },
+    }};
+    return ret;
+}
+
+Mat3x3 createRotationZMat3x3(float a) {
+    Mat3x3 ret = { .v = {
+        { cos(a), -sin(a), 0, },
+        { sin(a),  cos(a), 0, },
+        {      0,       0, 1, },
+    }};
+    return ret;
+}
+
+Mat3x3 createScaleMat3x3(float x, float y, float z) {
+    Mat3x3 ret = { .v = {
+        { x, 0, 0, },
+        { 0, y, 0, },
+        { 0, 0, z, },
+    }};
+    return ret;
+}
+
+Mat3x3 scaleMat3x3(Mat3x3 A, float s) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            A.v[i][j] *= s;
+        }
+    }
+    return A;
+}
+
+Mat3x3 multMat3x3(Mat3x3 A, Mat3x3 B) {
+    Mat3x3 ret;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            ret.v[i][j] = 0;
+            for (int k = 0; k < 3; k++) {
+                ret.v[i][j] += A.v[i][k] * B.v[k][j];
+            }
+        }
+    }
+    return ret;
+}
+
+Vec3 multMat3x3Vec3(Mat3x3 A, Vec3 v) {
+    Vec3 ret;
+    for (int i = 0; i < 3; i++) {
+        ret.v[i] = 0;
+        for (int k = 0; k < 3; k++) {
+            ret.v[i] += A.v[i][k] * v.v[k];
+        }
+    }
+    return ret;
+}
---
+++ src/vec.h
@@ -0,0 +1,47 @@
+#ifndef _VEC_H_
+#define _VEC_H_
+
+typedef union {
+    struct {
+        float x;
+        float y;
+        float z;
+    };
+    float v[3];
+} Vec3;
+
+typedef Vec3 Color;
+
+Vec3 createVec3(float x, float y, float z);
+
+Vec3 scaleVec3(Vec3 v, float s);
+
+Vec3 addVec3(Vec3 u, Vec3 v);
+
+float dotVec3(Vec3 u, Vec3 v);
+
+Vec3 crossVec3(Vec3 u, Vec3 v);
+
+typedef struct {
+    float v[3][3];
+} Mat3x3;
+
+Mat3x3 createNullMat3x3();
+
+Mat3x3 createIdentMat3x3();
+
+Mat3x3 createRotationXMat3x3(float a);
+
+Mat3x3 createRotationYMat3x3(float a);
+
+Mat3x3 createRotationZMat3x3(float a);
+
+Mat3x3 createScaleMat3x3(float x, float y, float z);
+
+Mat3x3 scaleMat3x3(Mat3x3 A, float s);
+
+Mat3x3 multMat3x3(Mat3x3 A, Mat3x3 B);
+
+Vec3 multMat3x3Vec3(Mat3x3 A, Vec3 v);
+
+#endif
